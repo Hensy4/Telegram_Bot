908e458d36c46f7f7c65319613afb5ec508d27e4
Umbrella <g.radim19@mail.ru>
2026-08-11 23:57:36 +0300

    fix: clean code without proxies

--- main.py
+++ main.py
@@ -7,31 +7,8 @@ from aiohttp_socks import ProxyConnector
 
 BOT_TOKEN = "8992122596:AAFZkWeWXeIqGRG6U_MwDspVjTQ1n7ES0Hc"
 
-# Бесплатные публичные прокси (могут работать нестабильно, но для теста ок)
-PROXY_LIST = [
-    'socks5://45.136.244.17:1080',
-    'socks5://46.8.28.35:1080',
-    'socks5://46.8.29.19:1080',
-    'socks5://85.203.17.66:1080',
-    'http://45.136.244.17:8080',
-]
 
-def get_proxy():
-    # Берем первый рабочий прокси из списка
-    for proxy in PROXY_LIST:
-        try:
-            return ProxyConnector.from_url(proxy)
-        except:
-            continue
-    return None
-
-proxy_connector = get_proxy()
-if proxy_connector:
-    session = AiohttpSession(connector=proxy_connector)
-    bot = Bot(token=BOT_TOKEN, session=session)
-else:
-    bot = Bot(token=BOT_TOKEN)  # fallback без прокси
-
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
