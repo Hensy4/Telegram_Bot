18260a34ec18449e5d62d8fea22e3e15fd31a59f
Umbrella <g.radim19@mail.ru>
2026-08-12 20:07:58 +0300

    add generate password

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import string
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
@@ -10,6 +12,29 @@ BOT_TOKEN = "8992122596:AAFZkWeWXeIqGRG6U_MwDspVjTQ1n7ES0Hc"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+
+# ====================
+# password start
+class PasswordGenerator:
+    def __init__(self):
+        self._password = ""
+        
+    def generate(self, length):
+        all_chars = string.digits + string.ascii_lowercase + string.ascii_uppercase + "!@#$%^&*()_+-="
+        self._password = ''.join(random.choice(all_chars) for _ in range(length))
+        return self._password
+
+password_gen = PasswordGenerator()
+
+@dp.message(Command("password"))
+async def handle_password(message: Message):
+    password = password_gen.generate(8)
+    await message.answer(f"Твой пароль: {password}")
+
+# ====================
+# password stop 
+
 
 @dp.message(Command("start"))
 async def handle_start(message: Message):
